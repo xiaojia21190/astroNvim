76c622c5369e5fa9c21169e8bf3257368c6deaf6
root <root@DESKTOP-N0N3RBK>
2023-08-14 13:50:50 +0800

    remove config

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,16 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.editing-support.nvim-regexplainer" },
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
 }
