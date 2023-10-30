ca00ac6e0d8fe7eda02c3111a42e434dec915957
jws <350331045@qq.com>
2023-10-30 12:31:41 +0800

    delete eslint

--- plugins/user.lua
+++ plugins/user.lua
@@ -5,13 +5,60 @@ return {
   {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
-    config = function()
-      require("lsp_signature").setup()
-    end,
+    config = function() require("lsp_signature").setup() end,
   },
   {
     "dstein64/vim-startuptime",
     -- lazy-load on a command
     cmd = "StartupTime",
+  },
+
+  {
+    "jay-babu/mason-null-ls.nvim",
+
+    opts = function(_, opts)
+      opts.ensure_installed = { "prettierd" }
+      if not opts.handlers then opts.handlers = {} end
+
+      local function check_json_key_exists(filename, key)
+        -- Open the file in read mode
+        local file = io.open(filename, "r")
+        if not file then
+          return false -- File doesn't exist or cannot be opened
+        end
+
+        -- Read the contents of the file
+        local content = file:read "*all"
+        file:close()
+
+        -- Parse the JSON content
+        local json_parsed, json = pcall(vim.fn.json_decode, content)
+        if not json_parsed or type(json) ~= "table" then
+          return false -- Invalid JSON format
+        end
+
+        -- Check if the key exists in the JSON object
+        return json[key] ~= nil
+      end
+
+      local has_prettier = function(util)
+        return check_json_key_exists(vim.fn.getcwd() .. "/package.json", "prettier")
+          or util.root_has_file ".prettierrc"
+          or util.root_has_file ".prettierrc.json"
+          or util.root_has_file ".prettierrc.yml"
+          or util.root_has_file ".prettierrc.yaml"
+          or util.root_has_file ".prettierrc.json5"
+          or util.root_has_file ".prettierrc.js"
+          or util.root_has_file ".prettierrc.cjs"
+          or util.root_has_file "prettier.config.js"
+          or util.root_has_file "prettier.config.cjs"
+          or util.root_has_file ".prettierrc.toml"
+      end
+
+      opts.handlers.prettierd = function()
+        local null_ls = require "null-ls"
+        null_ls.register(null_ls.builtins.formatting.prettierd.with { condition = has_prettier })
+      end
+    end,
   },
 }
